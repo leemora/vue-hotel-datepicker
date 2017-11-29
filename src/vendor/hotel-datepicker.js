edaa596781150b42733be60af564acc621752802
Lee <lee@Lees-MacBook-Pro.local>
2017-11-29 14:59:31 -0500

    BUG: Another global event listener making a lot of assumptions.

--- src/vendor/hotel-datepicker.js
+++ src/vendor/hotel-datepicker.js
@@ -526,7 +526,7 @@ export default class HotelDatepicker {
 
     documentHover(evt) {
         // Check if the hover is on a calendar day
-        if (evt.target.tagName && evt.target.tagName.toLowerCase() === 'td') {
+        if (evt.target.tagName && evt.target.tagName.toLowerCase() === 'td' && _.indexOf(evt.target.classList, 'datepicker__month-day') != -1) {
             this.dayHovering(evt.target);
         }
     }
